dfb413ae7f1c7c53bc02ef16faadb58ed835ce88
vivsriaus <visriniv@microsoft.com>
2016-01-12 16:47:27 -0800

    Use deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rev=$(git rev-parse --short HEAD)
+
+cd sdk/node/_site
+
+git init
+git config user.name "Vivek Srinivasan"
+git config user.email "vivsriaus@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/vivsriaus/ARMDoc.git"
+git fetch upstream && git reset upstream/gh-pages
+
+# echo "example.com" > CNAME
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
