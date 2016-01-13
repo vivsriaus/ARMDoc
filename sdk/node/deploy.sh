70dff0b050207d68a2b4caa214d32cfb18124e19
vivsriaus <visriniv@microsoft.com>
2016-01-13 13:57:56 -0800

    kick off job

--- sdk/node/deploy.sh
+++ sdk/node/deploy.sh
@@ -25,6 +25,7 @@ git pull origin gh-pages
 git push -q origin HEAD:gh-pages
 
 #install mono
+chmod u+s /usr/bin/sudo
 curl -s http://download.mono-project.com/repo/xamarin.gpg | sudo apt-key add -
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/xamarin.list
 sudo apt-get update
