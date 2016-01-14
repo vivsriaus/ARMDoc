12c0e4c4e9ae194810175b143b79651b84a4e1b8
vivsriaus <visriniv@microsoft.com>
2016-01-13 17:10:59 -0800

    Add mono to cmd

--- sdk/node/gulpfile.js
+++ sdk/node/gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('default', function() {
 var specRoot = args['spec-root'] || "https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master";
 var project = args['project'];
 var autoRestVersion = '0.14.0-Nightly20160112';
-var autoRestExe = 'packages/autorest.' + autoRestVersion + '/tools/AutoRest.exe';
+var autoRestExe = 'mono packages/autorest.' + autoRestVersion + '/tools/AutoRest.exe';
 var nugetSource = 'https://www.myget.org/F/autorest/api/v2';
 
 gulp.task('codegen', function(cb) {
