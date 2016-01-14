92b51eebdedba96cc51ca1f1c5f56a45430a6ce6
vivsriaus <visriniv@microsoft.com>
2016-01-14 14:08:16 -0800

    Add static files

--- sdk/node/deploy.sh
+++ sdk/node/deploy.sh
@@ -2,6 +2,10 @@
 
 rev=$(git rev-parse --short HEAD)
 
+cp -avr static/fonts _site/
+cp -avr static/scripts _site/
+cp -avr static/styles _site/
+
 cd _site
 
 git init
@@ -23,5 +27,3 @@ git add -A .
 git commit -m "rebuild pages at ${rev}"
 git pull origin gh-pages
 git push -q origin HEAD:gh-pages
-
-#install mono
