cf1d28702c6e2be997d20db860325fb70f2786c2
vivsriaus <visriniv@microsoft.com>
2016-01-20 15:20:53 -0800

    Add swagger ui files

---
+++ swagger-ui/publishSwaggerUI.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rev=$(git rev-parse --short HEAD)
+
+git init
+git config user.name "Vivek Srinivasan"
+git config user.email "vivsriaus@gmail.com"
+
+git remote add origin "https://$GH_TOKEN@github.com/vivsriaus/ARMDoc.git"
+git fetch origin
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git pull origin gh-pages
+git push -q origin HEAD:gh-pages
